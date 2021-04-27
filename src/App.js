faf4c2a0d040ec79ca87e35711dd70d703ca1c57
Debbie <sepp.der.bauer@alpenjodel.de>
2021-04-27 12:37:14 +0200

    setup project and first tries with store

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from './app/store'
 
 function App() {
+
+  // lokal in komponente
+  // const [hallo, setHallo] = useState(["a"]);
+
+  const hallo = useSelector(state => state.items);
+  const dispatch = useDispatch();
+
+  const currentCount = useSelector(state => state.counter);
+
+  useEffect(() => {
+    dispatch(addItem('c'));
+  }, []);
+
+  useEffect(() => {
+    console.log(hallo);
+  }, [hallo]);
+
+  const increaseCounter = () => {
+    dispatch({type: 'count up', counter: currentCount + 1})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <button onClick={increaseCounter}>Click me: {currentCount}</button>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
+// import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+const initialState = {
+  items: ["a"],
+  counter: 0
+}
+
+const actionTypes = { ADD: 'add', COUNTUP: 'count up' };
+
+export const addItem = (value) => {
+  return { type: actionTypes.ADD, value: value }
+};
+
+export const store = createStore((state, action) => {
+  switch (action.type) {
+    case actionTypes.ADD:
+      return {...state, items: state.items.concat(action.value)};
+    case actionTypes.COUNTUP:
+      return {...state, counter: action.counter};
+    default:
+      return state;
+  }
+
+  // if (action.type == 'add') {
+  //   return {...state, items: state.items.concat(action.value)};
+  // }
+  // if (action.type == 'count up') {
+  //   return {...state, counter: action.counter};
+  // }
+  // return state;
+}, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
